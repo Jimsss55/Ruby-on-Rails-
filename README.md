88d9f2c69a1dc82d52e61657f645b71870556d41
Jimpa <jamtshojimpa@gmail.com>
2024-12-24 12:07:43 +0600

    Add user table and user model

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Resources 
+- Users - Create user table and model
+        - Add validations
+Associations
+- One-to-many(Between users and articles)
 
-Things you may want to cover:
+REST for users 
 
-* Ruby version
+Authentication
+-Login using secure password 
+
+Restrictions for actions 
+-Based on logges in/logged out state
+
+Security 
+-Admin user functionality and access level 
+
+* Ruby version: ruby 3.3.6
 
 * System dependencies
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_16_114741) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_24_055429) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
