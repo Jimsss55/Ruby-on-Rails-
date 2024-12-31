2189b98de76b73a02c8f49bd373c7d0bc8167cdd
Jimpa <jamtshojimpa@gmail.com>
2024-12-31 11:49:19 +0600

    Add many to many association from backend

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :name, presence: true, length: { minimum: 6 }
   validates :description, presence: true, length: { minimum: 5, maximum: 1000 }
 end
--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(name: "jimpa", email: "jimpa@gmail.com",
+                              password: "Jimpa", admin: true)
+    sign_in_as(@admin_user)
+  end
+
   test "get new category from and create category" do
     get "/categories/new"
     assert_response :success
